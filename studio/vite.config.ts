00bf6af671d2d54e551127da3aa381a146ac0b75
poetic-design-net <hallo@frdrk.de>
2025-03-20 14:03:51 +0100

    build: add vite config with externals and react plugin

--- studio/vite.config.ts
+++ studio/vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@sanity/client']
+      external: [
+        '@sanity/client',
+        'sanity',
+        '@sanity/vision',
+        'sanity-plugin-media',
+        '@sanity/code-input',
+        '@sanity/table',
+        'sanity-plugin-asset-source-unsplash',
+        'styled-components'
+      ]
     }
   }
 })
