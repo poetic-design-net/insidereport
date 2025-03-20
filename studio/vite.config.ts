c1cffb48027623496f298c901a121f6b1160d68b
poetic-design-net <hallo@frdrk.de>
2025-03-20 14:07:04 +0100

    build: simplify vite config to handle @sanity/client

--- studio/vite.config.ts
+++ studio/vite.config.ts
@@ -1,20 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        '@sanity/client',
-        'sanity',
-        '@sanity/vision',
-        'sanity-plugin-media',
-        '@sanity/code-input',
-        '@sanity/table',
-        'sanity-plugin-asset-source-unsplash',
-        'styled-components'
-      ]
-    }
+  optimizeDeps: {
+    include: ['@sanity/client']
+  },
+  ssr: {
+    noExternal: ['@sanity/client']
   }
 })
